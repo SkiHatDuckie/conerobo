1b20f9934edd1b817e9355215c59320b8c8711ab
ConeRobo <skihatduckie@gmail.com>
2021-07-30 12:33:28 -0400

    Create blank window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use bevy::{
+    prelude::*,
+    render::pass::ClearColor
+};
+
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .insert_resource(WindowDescriptor {
+            title: "ConeRoboTracker".to_string(),
+            width: 480.,
+            height: 480.,
+            vsync: true,
+            ..Default::default()
+        })
+        .insert_resource(ClearColor(Color::rgb(0.00, 0.00, 0.00)))
+        .add_plugins(DefaultPlugins)
+        .run();
 }
