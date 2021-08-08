4ab43fb4bb3717d744dc8399173c674f72193144
ConeRobo <skihatduckie@gmail.com>
2021-08-08 15:21:48 -0400

    Create scoreboard/basic scoring

--- src/main.rs
+++ src/main.rs
@@ -31,16 +31,22 @@ fn main() {
     App::build()
         .add_plugin(WindowInitPlugin)
         .add_plugins(DefaultPlugins)
+        .insert_resource(Scoreboard { score: 0 })
         .add_startup_system(setup.system())
         .add_system(bevy::input::system::exit_on_esc_system.system())
         .add_system(paddle_movement_system.system())
         .add_system(ball_collision_system.system())
         .add_system(ball_movement_system.system())
+        .add_system(scoreboard_system.system())
         .run();
 }
 
 struct Paddle {
     speed: f32,
+}
+
+struct Scoreboard {
+    score: isize,
 }
 
 struct Ball {
@@ -49,12 +55,14 @@ struct Ball {
 
 enum Collider {
     Solid,
+    Bottom,
     Paddle,
 }
 
 fn setup(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
     // Add entities to the environment
     // Cameras
@@ -63,15 +71,15 @@ fn setup(
 
     // Paddle
     commands.spawn_bundle(SpriteBundle {
-        material: materials.add(Color::rgb_u8(245, 245, 175).into()),
-        transform: Transform::from_xyz(0.0, -245.0, 0.0),
+        material: materials.add(Color::rgb_u8(245, 200, 175).into()),
+        transform: Transform::from_xyz(0.0, -250.0, 0.0),
         sprite: Sprite::new(Vec2::new(100.0, 25.0)),
         ..Default::default()
     })
     .insert(Paddle { speed: 400.0 })
     .insert(Collider::Paddle);
 
-    // ball
+    // Ball
     commands.spawn_bundle(SpriteBundle {
         material: materials.add(Color::rgb_u8(245, 200, 175).into()),
         transform: Transform::from_xyz(0.0, -25.0, 1.0),
@@ -86,7 +94,7 @@ fn setup(
      let wall_material = materials.add(Color::rgb_u8(75, 185, 225).into());
      let wall_thickness = 10.0;
      let bounds = Vec2::new(WIN_WIDTH, WIN_HEIGHT);
-     // left
+     // Left
     commands.spawn_bundle(SpriteBundle {
         material: wall_material.clone(),
         transform: Transform::from_xyz(-bounds.x / 2.0, 0.0, 0.0),
@@ -94,7 +102,7 @@ fn setup(
         ..Default::default()
     })
     .insert(Collider::Solid);
-    // right
+    // Right
     commands.spawn_bundle(SpriteBundle {
         material: wall_material.clone(),
         transform: Transform::from_xyz(bounds.x / 2.0, 0.0, 0.0),
@@ -102,15 +110,16 @@ fn setup(
         ..Default::default()
     })
     .insert(Collider::Solid);
-    // bottom
+    // Bottom
     commands.spawn_bundle(SpriteBundle {
         material: wall_material.clone(),
         transform: Transform::from_xyz(0.0, -bounds.y / 2.0, 0.0),
         sprite: Sprite::new(Vec2::new(bounds.x + wall_thickness, wall_thickness)),
         ..Default::default()
     })
-    .insert(Collider::Solid);
-    // top
+    .insert(Collider::Solid)
+    .insert(Collider::Bottom);
+    // Top
     commands.spawn_bundle(SpriteBundle {
         material: wall_material,
         transform: Transform::from_xyz(0.0, bounds.y / 2.0, 0.0),
@@ -118,6 +127,41 @@ fn setup(
         ..Default::default()
     })
     .insert(Collider::Solid);
+
+    // Scoreboard
+    commands.spawn_bundle(TextBundle {
+        text: Text {
+            sections: vec![
+                TextSection {
+                    value: "Score: ".to_string(),
+                    style: TextStyle {
+                        font: asset_server.load("fonts\\EightBit Atari-Backtalk10.ttf"),
+                        font_size: 20.0,
+                        color: Color::rgb_u8(235, 235, 165),
+                    },
+                },
+                TextSection {
+                    value: "".to_string(),
+                    style: TextStyle {
+                        font: asset_server.load("fonts\\EightBit Atari-Backtalk10.ttf"),
+                        font_size: 20.0,
+                        color: Color::rgb_u8(235, 235, 165),
+                    },
+                },
+            ],
+            ..Default::default()
+        },
+        style: Style {
+            position_type: PositionType::Absolute,
+            position: Rect {
+                top: Val::Px(5.0),
+                left: Val::Px(5.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
 
 fn paddle_movement_system(
@@ -136,9 +180,9 @@ fn paddle_movement_system(
         }
 
         let translation = &mut transform.translation;
-        // move the paddle horizontally
+        // Move the paddle horizontally
         translation.x += time.delta_seconds() * direction * paddle.speed;
-        // bound the paddle within the walls
+        // Bound the paddle within the walls
         translation.x = translation.x.min(220.0).max(-220.0);
     }
 }
@@ -147,7 +191,7 @@ fn ball_movement_system(
     time: Res<Time>, 
     mut ball_query: Query<(&Ball, &mut Transform)>
 ) {
-    // clamp the timestep to stop the ball from escaping when the game starts
+    // Clamp the timestep to stop the ball from escaping when the game starts
     let delta_seconds = f32::min(0.2, time.delta_seconds());
 
     if let Ok((ball, mut transform)) = ball_query.single_mut() {
@@ -155,7 +199,16 @@ fn ball_movement_system(
     }
 }
 
+fn scoreboard_system(
+    scoreboard: Res<Scoreboard>, 
+    mut query: Query<&mut Text>
+) {
+    let mut text = query.single_mut().unwrap();
+    text.sections[0].value = format!("Score: {}", scoreboard.score);
+}
+
 fn ball_collision_system(
+    mut scoreboard: ResMut<Scoreboard>,
     mut ball_query: Query<(&mut Ball, &Transform, &Sprite)>,
     collider_query: Query<(&Collider, &Transform, &Sprite)>,
 ) {
@@ -163,7 +216,7 @@ fn ball_collision_system(
         let ball_size = sprite.size;
         let velocity = &mut ball.velocity;
 
-        // check collision with walls
+        // Check collision with walls
         for (collider, transform, sprite) in collider_query.iter() {
             let collision = collide(
                 ball_transform.translation,
@@ -172,12 +225,20 @@ fn ball_collision_system(
                 sprite.size,
             );
             if let Some(collision) = collision {
-                // reflect the ball when it collides
+                // Increment the scoreboard if ball collides with paddle (hit)
+                // Decrement if it collides with the bottom wall (miss)
+                if let Collider::Paddle = *collider {
+                    scoreboard.score += 1;
+                } else if let Collider::Bottom = *collider {
+                    scoreboard.score -= 1;
+                }
+
+                // Reflect the ball when it collides
                 let mut reflect_x = false;
                 let mut reflect_y = false;
 
-                // only reflect if the ball's velocity is going in the opposite direction of the
-                // collision
+                // Only reflect if the ball's velocity is going in the opposite direction of the
+                // Collision
                 match collision {
                     Collision::Left => reflect_x = velocity.x > 0.0,
                     Collision::Right => reflect_x = velocity.x < 0.0,
@@ -185,18 +246,18 @@ fn ball_collision_system(
                     Collision::Bottom => reflect_y = velocity.y > 0.0,
                 }
 
-                // reflect velocity on the x-axis if we hit something on the x-axis
+                // Reflect velocity on the x-axis if we hit something on the x-axis
                 if reflect_x {
                     velocity.x = -velocity.x;
                 }
 
-                // reflect velocity on the y-axis if we hit something on the y-axis
+                // Reflect velocity on the y-axis if we hit something on the y-axis
                 if reflect_y {
                     velocity.y = -velocity.y;
                 }
 
-                // break if this collide is on a solid, otherwise continue check whether a solid is
-                // also in collision
+                // Break if this collide is on a solid, otherwise continue check whether a solid is
+                // Also in collision
                 if let Collider::Solid = *collider {
                     break;
                 }
