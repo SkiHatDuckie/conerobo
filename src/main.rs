1d514147f6c36d10b6176a87bf4e017e28001100
ConeRobo <skihatduckie@gmail.com>
2021-08-26 22:09:35 -0400

    Optimizations and FPS check

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 mod capture;
 
 use capture::capture_screen;
+use std::time::Instant;
 
 fn main() {
     // Test screen capturing
-    capture_screen(100, 100, 300, 300);
+    let mut total_time: f32 = 0.0;
+    for _ in 1..=120 {
+        let loop_time = Instant::now();
+        capture_screen(0, 0, 1920, 1080);
+
+        // Add loop time to total
+        total_time += loop_time.elapsed().as_secs_f32()
+    }
+    println!("Total time: {}", total_time)
 }
