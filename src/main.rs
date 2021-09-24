cf51920624543e8d5b3d86553b7aa3f6d01de66b
WillyBBlue <skihatduckie@gmail.com>
2021-09-24 15:46:56 -0400

    Create a basic CUI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io::{
+    stdin, stdout, Write,
+};
+
 fn main() {
-    println!("Hello World!")
+    println!("=== ConeRobo ===");
+    let mut input = String::new();
+
+    loop {
+        print!(">");
+        stdout().flush().expect("Flush failed");
+
+        // Attempt to read user input
+        match stdin().read_line(&mut input) {
+            Ok(_) => (),
+            Err(err) => println!("Failed to parse input: {}", err)
+        }
+
+        // Process user input
+        match input.trim() {
+            "quit" => break,
+            _ => ()
+        }
+    }
 }
