746d8249f5d2dbc5debc1517ecdc4f0f9feadb48
ConeRobo <skihatduckie@gmail.com>
2022-09-17 23:10:34 -0400

    Add title and heading

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
+#![allow(non_snake_case)]
+// ^ Dioxus components use UpperCammelCase 
+
 use dioxus::prelude::*;
 
 fn main() {
-    dioxus::desktop::launch(app);
+    dioxus::desktop::launch_cfg(App, |c| c.with_window(|w| w.with_title("ConeRobo")));
 }
 
-fn app(cx: Scope) -> Element {
+fn App(cx: Scope) -> Element {
     cx.render(rsx! (
-        div { "Hello, world!" }
+        div {
+            h1 {
+                color: "rgb(200, 150, 60)",
+                "Welcome to ConeRobo" 
+            }
+            p {
+                "Hello, world!"
+            }
+        }
     ))
 }
-
-// Colors:
-// (26, 24, 36) Xiketic - Good for backgorund
-// (67, 66, 69) Onyx
-// (171, 177, 179) Silver Chalice
-// (241, 246, 255) Alice Blue - Good for background
-// (145, 210, 234)  Sky Blue
-// (73, 153, 187)  Blue Green - Good for background
-// (57, 67, 111)  Purple Navy
