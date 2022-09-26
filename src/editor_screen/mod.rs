b7d6bfdea6a25df943192869cf967bc39b2dfb8c
ConeRobo <skihatduckie@gmail.com>
2022-09-25 21:53:15 -0400

    Editor textarea updates on enter/backspace

--- src/editor_screen/mod.rs
+++ src/editor_screen/mod.rs
@@ -4,17 +4,31 @@
 use dioxus::prelude::*;
 
 #[inline_props]
-pub fn EditorScreen(cx: Scope, text: UseRef<String>) -> Element {
+pub fn EditorScreen(cx: Scope, text: UseRef<String>, num_rows: UseState<u32>) -> Element {
     let inner_text = &*text.read();
-
+    
     cx.render(rsx! (
         div {
             textarea {
-                rows: "1",
+                value: "{inner_text}",
+                rows: "{num_rows}",
                 cols: "50",
                 resize: "none",
+                onkeydown: move |evt| {
+                    match evt.key.as_str() {
+                        "Enter" => num_rows.modify(|n| n + 1),
+                        "Backspace" => {
+                            text.with(|i| {
+                                let lines: Vec<&str> = i.split('\n').collect::<Vec<&str>>();
+                                if lines[lines.len() - 1] == "" {
+                                    num_rows.modify(|n| n - 1)
+                                }
+                            })
+                        },
+                        _ => {}
+                    }
+                },
                 oninput: move |evt| text.set(evt.value.clone()),
-                "{inner_text}"
             }
         }
     ))
--- src/navigation_bar/mod.rs
+++ src/navigation_bar/mod.rs
@@ -7,17 +7,19 @@ use crate::editor_screen;
 
 pub fn NavigationBar(cx: Scope) -> Element {
     let active_screen = use_state(&cx, || 0);
-    // Temporarily storing the editor screen's text here.
+    // Temporarily storing the editor screen's states here.
     // Later on, I want to have some other component handle screen states when a screen isn't being rendered.
     let editor_text = use_ref(&cx, || "Hello!".to_owned());
+    let editor_num_rows = use_state(&cx, || 1);
 
     let screen = match active_screen.get() {
-        1 => { rsx!( welcome_screen::WelcomeScreen {
-            h1_color: "rgb(200, 160, 70)",
-            h2_color: "rgb(50, 70, 200)"
+        1 => { rsx!(welcome_screen::WelcomeScreen {
+                h1_color: "rgb(200, 160, 70)",
+                h2_color: "rgb(50, 70, 200)"
         })},
         2 => { rsx!( editor_screen::EditorScreen {
-            text: editor_text.clone()
+            text: editor_text.clone(),
+            num_rows: editor_num_rows.clone()
         })},
         _ => { rsx!( div {} ) }
     };
